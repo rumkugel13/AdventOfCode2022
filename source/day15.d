5035e47369913c4e61796a670ebae1538f1bf862
rumkugel13 <rumkugel13@outlook.com>
2024-01-01 22:09:57 +0100

    add day15 part 2

--- source/day15.d
+++ source/day15.d
@@ -15,6 +15,65 @@ void main(string[] args)
 
     auto part1 = beaconsCannotBeAt(sensorsAndBeacons, 2_000_000);
     writeln("part 1: ", part1);
+
+    auto part2 = beaconTuningFrequency(sensorsAndBeacons, 2 * 2_000_000);
+    writeln("part 2: ", part2);
+}
+
+long beaconTuningFrequency(SensorBeacon[] sensorsAndBeacons, int maxPos)
+{
+    Range[] ranges;
+    foreach (sb; sensorsAndBeacons)
+    {
+        auto dist = sb.sensor.manhattanDistance(sb.beacon) + 1;
+        auto dirs = [Point(1, 0), Point(0, 1), Point(-1, 0), Point(0, -1)];
+        for (auto i = 0; i < dirs.length; i++)
+        {
+            auto a = dirs[i];
+            auto b = dirs[(i + 1) % dirs.length];
+            Range range;
+            range.begin = sb.sensor + (a * dist);
+            range.end = sb.sensor + (b * dist);
+            ranges ~= range;
+        }
+    }
+
+    Point beacon;
+    outer: foreach (range; ranges)
+    {
+        auto len = max(abs(range.end.row - range.begin.row), abs(range.end.col - range.begin.col));
+        auto dir = (range.end - range.begin) / len;
+        for (int i = 1; i < len; i++)
+        {
+            bool found = true;
+            Point p = range.begin + dir * i;
+            if (!(p.row >= 0 && p.col >= 0 && p.row <= maxPos && p.col <= maxPos))
+            {
+                continue;
+            }
+            foreach (sb; sensorsAndBeacons)
+            {
+                auto reach = sb.sensor.manhattanDistance(sb.beacon);
+                auto dist = p.manhattanDistance(sb.sensor);
+                if (dist < reach)
+                {
+                    found = false;
+                    break;
+                }
+            }
+            if (found)
+            {
+                beacon = p;
+                break outer;
+            }
+        }
+    }
+    return cast(long) beacon.col * cast(long) maxPos + cast(long) beacon.row;
+}
+
+struct Range
+{
+    Point begin, end;
 }
 
 int beaconsCannotBeAt(SensorBeacon[] sensorsAndBeacons, int y)
--- source/util.d
+++ source/util.d
@@ -19,6 +19,14 @@ struct Point
             return Point(this.row - other.row, this.col - other.col);
     }
 
+    Point opBinary(string op)(int factor)
+    {
+        static if (op == "*")
+            return Point(this.row * factor, this.col * factor);
+        else static if (op == "/")
+            return Point(this.row / factor, this.col / factor);
+    }
+
     Point opUnary(string op)() if (op == "-")
     {
         return Point(-this.row, -this.col);
